471e175df4329c15d8404ad884a10b132b3879c0
kaleniksasha <kalenik.wr@gmail.com>
2018-09-11 08:33:46 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   let result = 0;
   for (let i = 0; i < preferences.length; i++) {
     let first = i;
+    if (preferences[first] === 0) continue;
+
     let second = preferences[first] - 1;
     if (first === second) continue;
+    if (preferences[second] === 0) continue;
+
     let third = preferences[second] - 1;
     if (second === third) continue;
+    if (preferences[third] === 0) continue;
+
     let thirdLoves = preferences[third] - 1;
+    console.log(first, second, third);
     if (thirdLoves === first) {
       preferences[first] = preferences[second] = preferences[third] = 0;
       result++;
     }
   }
+
   return result;
 };
